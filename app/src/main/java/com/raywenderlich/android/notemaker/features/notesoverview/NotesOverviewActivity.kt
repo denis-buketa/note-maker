5d03f6bb78d962edaf25f7f741b72434b6fcd4df
Denis Buketa <denis.buketa@fiveminutes.eu>
2019-07-12 23:46:26 +0200

    Created a starter project

--- app/src/main/java/com/raywenderlich/android/notemaker/features/notesoverview/NotesOverviewActivity.kt
+++ app/src/main/java/com/raywenderlich/android/notemaker/features/notesoverview/NotesOverviewActivity.kt
@@ -30,11 +30,7 @@
 package com.raywenderlich.android.notemaker.features.notesoverview
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.updatePadding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -55,12 +51,6 @@ class NotesOverviewActivity : AppCompatActivity(), NotesOverviewAdapter.OnNoteCl
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_notes_overview)
 
-    // Request to be layout full screen
-    requestToBeLayoutFullscreen()
-
-    // Adapt view according to insets
-    adaptViewForInsets()
-
     // Initialize toolbar with the title
     initToolbar()
 
@@ -77,40 +67,6 @@ class NotesOverviewActivity : AppCompatActivity(), NotesOverviewAdapter.OnNoteCl
   override fun onResume() {
     super.onResume()
     viewModel.fetchNotes()
-  }
-
-  private fun requestToBeLayoutFullscreen() {
-    root.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
-  }
-
-  private fun adaptViewForInsets() {
-
-    // Prepare original top padding of the toolbar
-    val toolbarOriginalTopPadding = toolbar.paddingTop
-
-    // Prepare original bottom margin of the "Add Note" button
-    val addNoteButtonMarginLayoutParam = addNoteButton.layoutParams as ViewGroup.MarginLayoutParams
-    val addNoteButtonOriginalBottomMargin = addNoteButtonMarginLayoutParam.bottomMargin
-
-    // Register OnApplyWindowInsetsListener
-    ViewCompat.setOnApplyWindowInsetsListener(root) { _, windowInsets ->
-
-      // Update toolbar's top padding to accommodate system window top inset
-      val newToolbarTopPadding = windowInsets.systemWindowInsetTop + toolbarOriginalTopPadding
-      toolbar.updatePadding(top = newToolbarTopPadding)
-
-      // Update "Add Note" button's bottom margin to accommodate system window bottom inset
-      addNoteButtonMarginLayoutParam.bottomMargin =
-          addNoteButtonOriginalBottomMargin + windowInsets.systemWindowInsetBottom
-      addNoteButton.layoutParams = addNoteButtonMarginLayoutParam
-
-      // Update notes recyclerView's bottom padding to accommodate system window bottom inset
-      notes.updatePadding(bottom = windowInsets.systemWindowInsetBottom)
-
-      windowInsets
-    }
-
   }
 
   private fun initToolbar() {
--- app/src/main/java/com/raywenderlich/android/notemaker/features/savenote/SaveNoteActivity.kt
+++ app/src/main/java/com/raywenderlich/android/notemaker/features/savenote/SaveNoteActivity.kt
@@ -31,22 +31,11 @@ package com.raywenderlich.android.notemaker.features.savenote
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Rect
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.doOnLayout
-import androidx.core.view.updatePadding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
 import com.raywenderlich.android.notemaker.R
 import com.raywenderlich.android.notemaker.data.model.Color
 import com.raywenderlich.android.notemaker.features.savenote.SaveNoteViewModel.Companion.INVALID_NOTE_ID
@@ -87,12 +76,6 @@ class SaveNoteActivity : AppCompatActivity(), ColorsAdapter.OnColorClickListener
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_save_note)
 
-    // Request to be layout full screen
-    requestToBeLayoutFullscreen()
-
-    // Adapt view according to insets
-    adaptViewForInsets()
-
     // Extract arguments (like note Id)
     extractArguments()
 
@@ -107,130 +90,6 @@ class SaveNoteActivity : AppCompatActivity(), ColorsAdapter.OnColorClickListener
 
     // Initialize view model, start observing LiveData, and request data
     initViewModel()
-  }
-
-  private fun requestToBeLayoutFullscreen() {
-    root.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
-  }
-
-  private fun adaptViewForInsets() {
-
-    // Prepare original top padding of the toolbar
-    val toolbarOriginalTopPadding = toolbar.paddingTop
-
-    // Prepare original peek height of the bottom sheet
-    val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-    val bottomSheetOriginalPeekHeight = bottomSheetBehavior.peekHeight
-
-    // Prepare original bottom margin of colors recycler view
-    val colorsLayoutParams = colors.layoutParams as ViewGroup.MarginLayoutParams
-    val colorsOriginalMarginBottom = colorsLayoutParams.bottomMargin
-
-    // Register OnApplyWindowInsetsListener
-    ViewCompat.setOnApplyWindowInsetsListener(root) { _, windowInsets ->
-
-      // Update toolbar's top padding to accommodate system window top inset
-      val newToolbarTopPadding = toolbarOriginalTopPadding + windowInsets.systemWindowInsetTop
-      toolbar.updatePadding(top = newToolbarTopPadding)
-
-      // Update colors recycler view's bottom margin to accommodate system window bottom inset
-      val newColorsMarginBottom = colorsOriginalMarginBottom + windowInsets.systemWindowInsetBottom
-      colorsLayoutParams.bottomMargin = newColorsMarginBottom
-      colors.layoutParams = colorsLayoutParams
-
-      // Update bottom sheet's peek height
-      adaptBottomSheetPeekHeight(bottomSheetBehavior, bottomSheetOriginalPeekHeight, windowInsets)
-
-      // Exclude gestures on colors recycler view when bottom sheet is expanded
-      excludeGesturesForColors(bottomSheetBehavior, windowInsets)
-
-      windowInsets
-    }
-  }
-
-  private fun adaptBottomSheetPeekHeight(
-      bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>,
-      bottomSheetOriginalPeekHeight: Int,
-      windowInsets: WindowInsetsCompat
-  ) {
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-
-      // If Q, update peek height according to gesture inset bottom
-      val gestureInsets = windowInsets.systemGestureInsets
-      bottomSheetBehavior.peekHeight = bottomSheetOriginalPeekHeight + gestureInsets.bottom
-
-    } else {
-
-      // If not Q, update peek height according to system window inset bottom
-      bottomSheetBehavior.peekHeight = bottomSheetOriginalPeekHeight + windowInsets.systemWindowInsetBottom
-    }
-  }
-
-  private fun excludeGesturesForColors(
-      bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>,
-      windowInsets: WindowInsetsCompat
-  ) {
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-
-      bottomSheetBehavior.setBottomSheetCallback(object :
-          BottomSheetBehavior.BottomSheetCallback() {
-
-        override fun onSlide(bottomSheet: View, slideOffset: Float) {
-          // NO OP
-        }
-
-        override fun onStateChanged(bottomSheet: View, newState: Int) {
-
-          if (newState == STATE_EXPANDED) {
-
-            // Exclude gestures when bottom sheet is expanded
-            root.doOnLayout {
-
-              val gestureInsets = windowInsets.systemGestureInsets
-
-              // Common Rect values
-              val rectHeight = colors.height
-              val rectTop = root.bottom - rectHeight
-              val rectBottom = root.bottom
-
-              // Left Rect values
-              val leftExclusionRectLeft = 0
-              val leftExclusionRectRight = gestureInsets.left
-
-              // Right Rect values
-              val rightExclusionRectLeft = root.right - gestureInsets.right
-              val rightExclusionRectRight = root.right
-
-              // Rect for gestures on the left side of the screen
-              val leftExclusionRect = Rect(
-                  leftExclusionRectLeft,
-                  rectTop,
-                  leftExclusionRectRight,
-                  rectBottom
-              )
-
-              // Rect for gestures on the right side of the screen
-              val rightExclusionRect = Rect(
-                  rightExclusionRectLeft,
-                  rectTop,
-                  rightExclusionRectRight,
-                  rectBottom
-              )
-
-              // Add both rects and exclude gestures
-              root.systemGestureExclusionRects = listOf(leftExclusionRect, rightExclusionRect)
-            }
-          } else if (newState == STATE_EXPANDED) {
-
-            // Remove exclusion rects when bottom sheet is collapsed
-            root.doOnLayout { root.systemGestureExclusionRects = listOf() }
-          }
-        }
-      })
-    }
   }
 
   private fun extractArguments() {
